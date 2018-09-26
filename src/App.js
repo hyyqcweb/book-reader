59f472fc7ec1a61413d54a2cd77e708c6f4ea6f7
hyyqc <junjie_hyyqc@163.com>
2018-09-26 15:30:02 +0800

    配置less, antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './style.less'
+import {Button} from 'antd'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         123
+        <Button>青纹</Button>
       </div>
     );
   }
