28bc208d294727b053f8df1d153be005b831c2ae
hyyqc <junjie_hyyqc@163.com>
2018-09-29 10:58:50 +0800

    fix

--- src/components/EmptyList/index.js
+++ src/components/EmptyList/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.less';
 
 export default ( {data} ) => {
-
     return (
         <div className="container">
             {data ? '暂无搜索结果' : '书架空空如也,快去添加书籍吧'}
--- src/config/utils.js
+++ src/config/utils.js
@@ -20,7 +20,6 @@ export const WordConversion = ({data, wordCount, search}) => {
                 wordage = wordCount + '字';
             }
         }
-
     }
 
     return (
